615b067cc14b158f8772e2769db880bbe5ee3a9b
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-06-25 20:55:18 +0300

    added a constructor for pizza details

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
  //Pizza object encapsulation //
  let periSmall = {
     pizzaName: "Peri-peri Chicken",
-    pizzaCost: "small",
+    pizzaSize: "small",
     pizzaCost: 550,
   }; 
   let periMedium = {
     pizzaName: "Peri-peri Chicken",
-    pizzaCost: "medium",
+    pizzaSize: "medium",
     pizzaCost: 800,
   };
   let periLarge= {
     pizzaName: "Peri-peri Chicken",
-    pizzaCost: "large",
+    pizzaSize: "large",
     pizzaCost: 1200,
   };
+//Using a constructor on pizza//
+function pizzaDetails (name,size,cost){
+    this.name=name;
+    this.size=size;
+    this.cost=cost;
+}
+let periSmall = new ("periperiChicken","small",550);
