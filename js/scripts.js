2796e4d607472eede28d690960cbce019c1c6656
George-ongaya1git config --global user.email georgeongaya94.gmail.comgit config --golabl user.name George-ongaya1 <georgeongaya94@gmail.com>
2021-06-27 21:01:06 +0300

    added functions to check the price

--- js/scripts.js
+++ js/scripts.js
@@ -1,203 +1,58 @@
-$(document).ready(function() {
-
-    $("#add-address").click(function() {
-      $("#new-addresses").append('<div class="new-address">' +
-                                   '<div class="form-group">' +
-                                     '<label for="new-street">Street</label>' +
-                                     '<input type="text" class="form-control new-street">' +
-                                   '</div>' +
-                                   '<div class="form-group">' +
-                                     '<label for="new-city">City</label>' +
-                                     '<input type="text" class="form-control new-city">' +
-                                   '</div>' +
-                                   '<div class="form-group">' +
-                                     '<label for="new-county">County</label>' +
-                                     '<input type="text" class="form-control new-county">' +
-                                   '</div>' +
-                                 '</div>');
-    });
-  
-    $("form#new-contact").submit(function(event) {
-      event.preventDefault();
-  
-      var inputtedFirstName = $("input#new-first-name").val();
-      var inputtedLastName = $("input#new-last-name").val();
-      var newContact = new Contact(inputtedFirstName, inputtedLastName);
-  
-      $(".new-address").each(function() {
-        var inputtedStreet = $(this).find("input.new-street").val();
-        var inputtedCity = $(this).find("input.new-city").val();
-        var inputtedCounty = $(this).find("input.new-county").val();
-        var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty)
-        newContact.addresses.push(newAddress)
-      });
-  
-      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-  
-      $(".contact").last().click(function() {
-        $("#show-contact").show();
-        $("#show-contact h2").text(newContact.fullName());
-        $(".first-name").text(newContact.firstName);
-        $(".last-name").text(newContact.lastName);
-        $("ul#addresses").text("");
-        newContact.addresses.forEach(function(address) {
-          $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.county + "</li>");
-        });
-      });
-  
-      $("input#new-first-name").val("");
-      $("input#new-last-name").val("");
-      $("input.new-street").val("");
-      $("input.new-city").val("");
-      $("input.new-county").val("");
-  
-    });
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-//Pizza object encapsulation //
-let periSmall = {
-    pizzaName: "Peri-peri Chicken",
-    pizzaSize: "small",
-    pizzaCost: 550,
-};
-let periMedium = {
-    pizzaName: "Peri-peri Chicken",
-    pizzaSize: "medium",
-    pizzaCost: 800,
-};
-let periLarge = {
-    pizzaName: "Peri-peri Chicken",
-    pizzaSize: "large",
-    pizzaCost: 1200,
-};
-//Using a prototype on pizza//
-pizzaDetails.prototype.peri = function () { return this + "" }
-
-
-//Using a constructor on pizza//
-function pizzaDetails(name, size, cost) {
-    this.name = name;
-    this.size = size;
-    this.cost = cost;
-}
-let periSmall = new ("periperiChicken");
-let periMedium = new ("periperiChicken", "medium", 800);
-let periLarge = new ("periperiChicken", "large", 1200);
-
-let vegSmall = new ("Veg Tikka", "small", 550);
-let vegMedium = new ("Veg Tikka", "medium", 800);
-let vegLarge = new ("Veg Tikka", "large", 1200);
-
-
-
-/*
-<p>Types of pizza </p>
-    <ul>
-        <li> Peri-peri Chicken</li>
-        <li> Regina</li>
-        <li> Veg Tikka</li>
-        <li> BBQ Steak</li>
-        <li> Boerewors </li>
-        <li> Chicken and Mushroom </li>
-        <li> Chicken Tikka </li>
-        <li> Hawaiian </li>
-    </ul>
-    <p>Size of pizza </p>
-    <ul>
-        <li>Small</li>
-        <li> Medium</li>
-        <li> Large </li>
-    </ul>
-    <p>Pictures of pizza </p>
-    <ul>
-        <li>
-            <figure><img src="images/Peri-peri Chicken.jpeg">
-                <figcaption> Peri-peri Chicken</figcaption>
-            </figure>
-        </li>
-        <li>
-            <figure><img src="images/Regina.jpeg">
-                <figcaption>Regina</figcaption>
-            </figure>
-        </li>
-        <li>
-            <figure><img src="images/Veg Tikka.jpeg">
-                <figcaption> Veg Tikka</figcaption>
-            </figure>
-        </li>
-        <li>
-            <figure><img src="images/BBQ Steak.jpeg">
-                <figcaption> BBQ Steak</figcaption>
-            </figure>
-        </li>
-        <li>
-            <figure><img src="images/Boerewors.jpeg">
-                <figcaption> Boerewors</figcaption>
-            </figure>
-        </li>
-        <li>
-            <figure><img src="images/Chicken and Mushroom.jpeg">
-                <figcaption> Chicken and Mushroom</figcaption>
-            </figure>
-        </li>
-        <li>
-            <figure><img src="images/Chicken Tikka.jpeg">
-                <figcaption>Chicken Tikka</figcaption>
-            </figure>
-        </li>
-        <li>
-            <figure><img src="images/Hawaiian.jpeg">
-                <figcaption> Hawaiian</figcaption>
-            </figure>
-        </li>
-    </ul>
-
-    <p>Cost of pizza </p>
-    <ul>
-        <li>Small - 550 kes</li>
-        <li> Medium - 800 kes</li>
-        <li> Large - 1200 kes</li>
-    </ul>
-    <p>Types of pizza crust </p>
-    <ul>
-        <li> Thin crust</li>
-        <li> Thick crust</li>
-    </ul>
-    <p>Types of pizza toppings</p>
-    <ul>
-        <li> Pepperoni</li>
-        <li> Mushroom</li>
-        <li> Extra cheese</li>
-        <li> Sausage</li>
-        <li> Onion</li>
-        <li> Black olives</li>
-        <li> Green pepper </li>
-        <li> Fresh garlic </li>
-        <li> Tomato</li>
-        <li> Fresh basil</li>
-    </ul>
-    <p>Cost of pizza toppings</p>
-    <ul>
-        <li> Pepperoni - 50 kes</li>
-        <li> Mushroom - 60 kes</li>
-        <li> Extra cheese - 70 kes</li>
-        <li> Sausage - 80 kes</li>
-        <li> Onion - 50 kes</li>
-        <li> Black olives - 60 kes</li>
-        <li> Green pepper - 50 kes</li>
-        <li> Fresh garlic - 70 kes</li>
-        <li> Tomato - 60 kes</li>
-        <li> Fresh basil -80 kes</li>
-    </ul>
-    */
+$(function () {
+    $('.summary').hide();
+    $('.cdata-overlay').hide();
+    //Get inputs
+    $("#checkout").click(function () {
+        let flavour = $(".flavour option:selected").val();
+        let size = $("#size option:selected").val();
+        let crust = $("#crust option:selected").val();
+        let topping = $("#toppings option:selected").val();
+        let number = $("#number").val();
+        console.log(size);
+        //Function order
+        let order = (f, s, c, t, n, total) => {
+            return { f, s, c, t, n, total };
+        };
+  //check price
+  let price, totalPrice;
+  switch (flavour) {
+      case flavour = "vegtikka":
+          switch (size) {
+              case size = "regular":
+                  price = 300;
+                  if (crust === "thin") {
+                      totalPrice = (price * number) + 100;
+                  } else if (crust === "thick") {
+                      totalPrice = (price * number) + 150;
+                  } else if (crust === "flatbread") {
+                      totalPrice = (price * number) + 180;
+                  } else {
+                      totalPrice = (price * number) + 280;
+                  }
+                  break;
+              case size = "medium":
+                  price = 600;
+                  if (crust === "thin") {
+                      totalPrice = (price * number) + 100;
+                  } else if (crust === "thick") {
+                      totalPrice = (price * number) + 150;
+                  } else if (crust === "flatbread") {
+                      totalPrice = (price * number) + 180;
+                  } else {
+                      totalPrice = (price * number) + 280;
+                  }
+                  break;
+              case size = "large":
+                  price = 1200;
+                  if (crust === "thin") {
+                      totalPrice = (price * number) + 100;
+                  } else if (crust === "thick") {
+                      totalPrice = (price * number) + 150;
+                  } else if (crust === "flatbread") {
+                      totalPrice = (price * number) + 180;
+                  } else {
+                      totalPrice = (price * number) + 280;
+                  }
+                  break;
+          }
+          break;
